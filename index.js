b31362ad57e7bacb1635b88ec4d6a336fa70f5fe
Jhon Torres <jhetorresto@unal.edu.co>
2018-03-18 13:06:01 -0500

    first version

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!!!');
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.sendStatus(err.status || 500);
+});
+
+app.listen(3001, () => { console.log(`Server is running on port ${PORT}`)});
